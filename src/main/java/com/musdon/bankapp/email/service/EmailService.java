d8906658891cbc97d7a272eafb689c8dbecc3cba
Saurabh <celtoxx@gmail.com>
2023-07-23 23:57:12 +0530

    updates

--- src/main/java/com/musdon/bankapp/email/service/EmailService.java
+++
@@ -1,9 +0,0 @@
-package com.musdon.bankapp.email.service;
-
-import com.musdon.bankapp.email.dto.EmailDetails;
-
-public interface EmailService {
-    String sendSimpleEmail(EmailDetails emailDetails);
-    String sendEmailWithAttachment(EmailDetails emailDetails);
-
-}
